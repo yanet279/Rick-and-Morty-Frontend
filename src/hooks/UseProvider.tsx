9fe7a0c152f8ac6578bfb522fae2247eef12c799
Yanet Choque <sol3127g@gmaill.com>
2024-11-15 22:07:23 -0300

    cambios en useProvider

--- src/hooks/UseProvider.tsx
+++ src/hooks/UseProvider.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import Context from "../context/provider";
+import Context from "../context/Provider";
 
 const UseProject = () => {
     return useContext(Context);
